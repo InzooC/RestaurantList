7441e571f8de52b5fb8e54a127ff64ae965d1e8b
InzooC <bluefreaks@gmail.com>
2022-03-09 20:05:14 +0800

    feat: modify index.handlebars

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant')//載入Restaurant model
+
 mongoose.connect('mongodb://localhost/restaurant')
 
 //取得資料庫連線狀態
@@ -23,23 +25,26 @@ app.use(express.static('public'))
 
 //setting router
 app.get('/', (req, res) => {
-  res.render('index', { restaurantList: restaurantList.results })
+  Restaurant.find()//拿出Restaurant model所有東西
+    .lean()
+    .then(restaurants => res.render('index', { restaurants: restaurants }))
+    .catch(error => console.log(error))
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant =>
-    restaurant.id.toString() === req.params.restaurant_id
-  )
-  res.render('show', { restaurant: restaurant })
-})
+// app.get('/restaurants/:restaurant_id', (req, res) => {
+//   const restaurant = restaurantList.results.find(restaurant =>
+//     restaurant.id.toString() === req.params.restaurant_id
+//   )
+//   res.render('show', { restaurant: restaurant })
+// })
 
-app.get('/search', (req, res) => {
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase().trim()) || restaurant.category.toLowerCase().includes(req.query.keyword.toLowerCase().trim())
-  })
-  const keyword = req.query.keyword
-  res.render('index', { restaurantList: restaurants, keyword: keyword })
-})
+// app.get('/search', (req, res) => {
+//   const restaurants = restaurantList.results.filter(restaurant => {
+//     return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase().trim()) || restaurant.category.toLowerCase().includes(req.query.keyword.toLowerCase().trim())
+//   })
+//   const keyword = req.query.keyword
+//   res.render('index', { restaurantList: restaurants, keyword: keyword })
+// })
 
 //start and listen on the express server> 
 app.listen(port, () => {
