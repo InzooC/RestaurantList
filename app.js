5e42bc24d8bf6d50d6fd1822f5e297c6b73da6af
InzooC <bluefreaks@gmail.com>
2022-03-10 20:53:20 +0800

    feat: add body-parser and add update restaurant function

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const restaurant = require('./models/restaurant')
 const Restaurant = require('./models/restaurant')//載入Restaurant model
 
 mongoose.connect('mongodb://localhost/restaurant')
@@ -22,20 +24,51 @@ app.set('view engine', 'handlebars')
 
 //setting static files
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 //setting router
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //瀏覽全部餐廳
   Restaurant.find()//拿出Restaurant model所有東西
     .lean()
     .then(restaurants => res.render('index', { restaurants: restaurants }))
     .catch(error => console.log(error))
 })
 
-app.get('/restaurant/:id', (req, res) => {
+app.get('/restaurant/:id', (req, res) => { //瀏覽一家餐廳詳細資訊
   const id = req.params.id
   return Restaurant.findById(id)
     .lean()
     .then((restaurant) => res.render('show', { restaurant: restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.get('/restaurant/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant: restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurant/:id/edit', (req, res) => {
+  const id = req.params.id
+  // console.log('request', req)
+  const name = req.body.name
+  const category = req.body.category
+  const location = req.body.location
+  const phone = req.body.phone
+  const description = req.body.description
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name
+      restaurant.category = category
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurant/${id}`))
     .catch(error => console.log(error))
 })
 
@@ -47,7 +80,7 @@ app.get('/restaurant/:id', (req, res) => {
 //   res.render('index', { restaurantList: restaurants, keyword: keyword })
 // })
 
-//start and listen on the express server> 
+//start and listen on the express server 
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
 })
